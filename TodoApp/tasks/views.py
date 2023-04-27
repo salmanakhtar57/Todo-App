cdc548084a0beb9021eb29ccd13ef3a47b9a0a6c
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-27 20:00:39 +0500

    craeted forms in views.py

--- TodoApp/tasks/views.py
+++ TodoApp/tasks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django import forms
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    
 tasks = ["foo", "bar", "baz"]
 
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {"form": NewTaskForm()
+})
