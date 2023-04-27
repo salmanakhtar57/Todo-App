73fa2b3d59f5f3b3c03c1481358c4fc7b2e4fbbd
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-27 20:38:02 +0500

    resolving errors

--- TodoApp/tasks/views.py
+++ TodoApp/tasks/views.py
@@ -6,30 +6,21 @@ from django.urls import reverse
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     
-tasks = []
-
 def index(request):
-    if tasks not in request.session:
-        request.session["tasks"] = []
-
-    return render(request, "tasks/index.html", {
-        "tasks": request.session["tasks"]
-    })
-
+    tasks = request.session.get("tasks", [])
+    return render(request, "tasks/index.html", {"tasks": tasks})
 
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            request.session["tasks"] += [task]
-            task.append(task)
+            tasks = request.session.get("tasks", [])
+            tasks.append(task)
+            request.session["tasks"] = tasks
             return HttpResponseRedirect(reverse("tasks:index"))
-        else:
-            return render(request, "tasks/add.html", {
-                 "form": form
-                 })
-        
-    return render(request, "tasks/add.html", {
-        "form": NewTaskForm()
-        })
+    else:
+        form = NewTaskForm()
+
+    return render(request, "tasks/add.html", {"form": form})
+
