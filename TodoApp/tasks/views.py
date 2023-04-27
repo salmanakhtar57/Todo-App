2b40beb9cb3eafcfef4bb25a6654dddabe174988
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-27 20:18:17 +0500

    complete the project

--- TodoApp/tasks/views.py
+++ TodoApp/tasks/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
     
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 def index(request):
-    return render(request, "tasks/index.html", {"tasks": tasks})
+    if tasks not in request.session:
+        request.session["tasks"] = []
+
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
 
 def add(request):
-    return render(request, "tasks/add.html", {"form": NewTaskForm()
-})
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            task.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                 "form": form
+                 })
+        
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+        })
