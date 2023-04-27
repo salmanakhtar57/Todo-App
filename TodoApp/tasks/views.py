35c0ccdd155964bc4380d434d8768909d3132712
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-27 16:03:30 +0500

    created a sample task list with render tamplate

--- TodoApp/tasks/views.py
+++ TodoApp/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+tasks = ["Python", "Django", "Flask"]
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
